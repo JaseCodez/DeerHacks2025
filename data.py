100acea9e48685c26d8bee138c0c8b1faca9f4f8
kimjere5 <jeremydaniel.kim@mail.utoronto.ca>
2025-02-15 23:07:49 -0500

    added prompts to LLM

--- data.py
+++ data.py
@@ -3,12 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
+
 client = OpenAI()
 response = client.chat.completions.create(
-model="gpt-3.5-turbo",
+model="gpt-4",
 messages=[
     {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Give me 10 ideas for a Youtube channel about Python"},
+    {"role": "user", "content": "I am going to Richmond Hill, Ontario starting from February 16-20. For each day, schedule what events/places I should do ONLY in Richmond Hill from 11am-11pm. Please provide the response in JSON format"},
     ]
 )
 
